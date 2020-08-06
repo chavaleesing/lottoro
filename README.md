aa9abfa47107fb01aa7abf70e6544c0181295cc3
chavaleesing <chavalee.sing@gmail.com>
2020-08-06 11:25:58 +0700

    update readme and refac some code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # lottoro
+
+### Quick Start to use API:
+
+Build image
+
+`docker build -t lottoro-app .`
+
+Run container
+
+`docker run -d --name python-lottoro-app -p 5000:5000 lottoro-app`
+
+---
+
+Example request in Postman: https://www.getpostman.com/collections/d9096443fb2ba35b7a6c
--- app.py
+++ app.py
@@ -12,16 +12,18 @@ lottery_result = random_lottery_result()
 
 @app.route('/lotto/showResult', methods=['GET'])
 def show_lottery_result():
-    data = get_win_user(lottery_result)
-    if data:
-        result_msg = f'Congrats !!! {data} You won the lottery prize :)'
-    else:
-        result_msg = f'Unfortunately, The lottery took your money. No one win this prize T_T'
-    return {
-        "result_msg": result_msg,
-        "result_number": f'The Lottery result is: {lottery_result}'
-    }
-
+    try:
+        data = get_win_user(lottery_result)
+        if data:
+            result_msg = f'Congrats !!! {data} You won the lottery prize :)'
+        else:
+            result_msg = f'Unfortunately, The lottery took your money. No one win this prize T_T'
+        return {
+            "status": "success",
+            "data": result_msg,
+            "result_number": f'The Lottery result is: {lottery_result}'}
+    except Exception as e:
+        return {"error": str(e), "status": "error"}, 400
 
 @app.route('/lotto/buy', methods=['POST'])
 def lottery_buying():
@@ -31,9 +33,12 @@ def lottery_buying():
         lottery_number = req_data["lottery_number"]
         if validate_lottery_number(lottery_number):
             add_lottery_buying(name, lottery_number)
-            result = {"result": f"User {name} bought number: {lottery_number} successfully."}
+            return {
+                "data": f"User {name} bought number: {lottery_number} successfully.", 
+                "status": "success"}
         else:
-            result = {"error": f"Sorry, Lotto service sell only 2 digits , Pls try ex: 82, 09, 33 .."}
+            return {
+                "error": f"Sorry, Lotto service sell only 2 digits lottery , Pls try ex: 82, 09, 33 ..", 
+                "status": "error"}, 400
     except Exception as e:
-        result = {"error": str(e)}
-    return result
+        return {"error": str(e), "status": "error"}, 400
