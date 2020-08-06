9ca386f92da416ede0cf3dce7fb7e90c2f9d6fa3
chavaleesing <chavalee.sing@gmail.com>
2020-08-06 10:13:56 +0700

    add function to find win user name

--- provider.py
+++ provider.py
@@ -7,4 +7,14 @@ def random_lottery_result():
     lottery_result = f"{rand_number:06}"
     return lottery_result
 
-
+def get_win_user(lottery_result):
+    two_digits_result = lottery_result[-2:]
+    conn = db_create_connection()
+    cur = conn.cursor()
+    cur.execute(f"select name from lottery_buying where lottery_number like '%{two_digits_result}' ")
+    data = cur.fetchall()
+    conn.close()
+    win_user = None
+    if data:
+        win_user = ", ".join({d[0] for d in data})  
+    return win_user
