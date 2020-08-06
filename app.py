c03c0599bc88ffcd01a309ce755b9370ff353c82
chavaleesing <chavalee.sing@gmail.com>
2020-08-06 10:56:39 +0700

    add try, exception and add lottery buying function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import Flask, jsonify, request
-from provider import get_win_user, random_lottery_result
+from provider import get_win_user, random_lottery_result, add_lottery_buying, validate_lottery_number
 from db import db_create_connection, db_initial_table
 
 
@@ -14,7 +14,7 @@ lottery_result = random_lottery_result()
 def show_lottery_result():
     data = get_win_user(lottery_result)
     if data:
-        result_msg = f'Congrats !!! K.{data} You won lottery Prize :)'
+        result_msg = f'Congrats !!! {data} You won the lottery prize :)'
     else:
         result_msg = f'Unfortunately, The lottery took your money. No one win this prize T_T'
     return {
@@ -25,16 +25,16 @@ def show_lottery_result():
 
 @app.route('/lotto/buy', methods=['POST'])
 def lottery_buying():
-    req_data = request.get_json()
-    name = req_data["name"]
-    lottery_number = req_data["lottery_number"]
-    conn = db_create_connection()
-    cur = conn.cursor()
     try:
-        cur.execute(f"INSERT INTO lottery_buying (name,lottery_number) VALUES('{name}','{lottery_number}');")
-        conn.commit()
-        result = {"result": f"K.{name} bought {lottery_number} successfully"}
+        req_data = request.get_json()
+        name = req_data["name"]
+        lottery_number = req_data["lottery_number"]
+        if validate_lottery_number(lottery_number):
+            add_lottery_buying(name, lottery_number)
+            result = {"result": f"User {name} bought number: {lottery_number} successfully."}
+        else:
+            result = {"error": f"Sorry, Lotto service sell only 2 digits , Pls try ex: 82, 09, 33 .."}
     except Exception as e:
         result = {"error": str(e)}
-    conn.close()
+        raise e
     return result
--- provider.py
+++ provider.py
@@ -8,13 +8,31 @@ def random_lottery_result():
     return lottery_result
 
 def get_win_user(lottery_result):
-    two_digits_result = lottery_result[-2:]
-    conn = db_create_connection()
-    cur = conn.cursor()
-    cur.execute(f"select name from lottery_buying where lottery_number like '%{two_digits_result}' ")
-    data = cur.fetchall()
-    conn.close()
-    win_user = None
-    if data:
-        win_user = ", ".join({d[0] for d in data})  
-    return win_user
+    try:
+        two_digits_result = lottery_result[-2:]
+        conn = db_create_connection()
+        cur = conn.cursor()
+        cur.execute(f"select name from lottery_buying where lottery_number like '%{two_digits_result}' ")
+        data = cur.fetchall()
+        win_user = None
+        if data:
+            win_user = ", ".join({"K."+d[0] for d in data})  
+        return win_user
+    except Exception as e:
+        raise e
+    finally:
+        conn.close()
+
+def add_lottery_buying(name, lottery_number):
+    try:
+        conn = db_create_connection()
+        cur = conn.cursor()
+        cur.execute(f"INSERT INTO lottery_buying (name,lottery_number) VALUES('{name}','{lottery_number}');")
+        conn.commit()
+    except Exception as e:
+        raise e
+    finally:
+        conn.close()
+
+def validate_lottery_number(lottery_number):
+    return lottery_number.isdigit() and len(lottery_number) == 2
